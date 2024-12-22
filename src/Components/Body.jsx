88dd46250f00b70de66b0bb16592791ee0835710
Arunav Singh <arunavsingh999@gmail.com>
2024-12-22 18:04:36 +0530

    added components

---
+++ src/Components/Body.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import BodyCss from "./Body.module.css";
+
+const Body = () => {
+  return <p className={BodyCss.color}>I am Body</p>;
+};
+
+export default Body;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HeaderCSS from "./Header.module.css";
+
+const Header = () => {
+  return <h2 className={HeaderCSS.color}>I am a header</h2>;
+};
+
+export default Header;
