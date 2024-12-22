d4dc6fb174bec94f2084d58ca941664c8384c728
Arunav Singh <arunavsingh999@gmail.com>
2024-12-23 00:47:02 +0530

    design done for search box and icon

--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Axios from "axios";
-import BodyCss from "./SearchBox.module.css";
+import SearchBoxCss from "./SearchBox.module.css";
 
 // const Body = () => {
 //   const [pokemonName, setPokemonName] = useState("");
@@ -89,7 +89,26 @@ import BodyCss from "./SearchBox.module.css";
 const SearchBox = () => {
   return (
     <>
-      <div></div>
+      <div className={SearchBoxCss.body}>
+        <div className={SearchBoxCss.search}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            className={SearchBoxCss.icon}
+          >
+            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+          </svg>
+          <input
+            type="text"
+            placeholder="Enter Pokemon Name"
+            className={SearchBoxCss.input}
+          />
+        </div>
+
+        <div>
+          <button>Search</button>
+        </div>
+      </div>
     </>
   );
 };
