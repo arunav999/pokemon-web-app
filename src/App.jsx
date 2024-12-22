cf140f5ff9053a460d4f51ce7cf06d7a3f8c4ab6
Arunav Singh <arunavsingh999@gmail.com>
2024-12-22 18:04:18 +0530

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import React from "react";
+import Header from "./Components/Header";
+import Body from "./Components/Body";
+
 function App() {
   return (
     <>
       <h1>Pokemon Web App</h1>
+      <Header />
+      <Body />
     </>
   );
 }
