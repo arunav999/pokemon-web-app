dca9f01ed7cafa159812e51896f9e401a66aff18
Arunav Singh <arunavsingh999@gmail.com>
2024-12-24 21:42:47 +0530

    poke card added

---
+++ src/Components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PokeCard from "./PokemonCard.module.css";
+
+const PokemonCard = () => {
+  return (
+    <>
+      <div className={PokeCard.card}>hello card</div>
+    </>
+  );
+};
+
+export default PokemonCard;
