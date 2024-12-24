2d1acb7c6fed143414fd4b0d11ead7374c749daf
Arunav Singh <arunavsingh999@gmail.com>
2024-12-24 23:19:10 +0530

    layout design
    desigend the layout now giving css design

--- src/Components/Contents/PokemonCard.jsx
+++ src/Components/Contents/PokemonCard.jsx
@@ -4,8 +4,21 @@ import PokeCard from "./PokemonCard.module.css";
 const PokemonCard = () => {
   return (
     <>
-      <div className={PokeCard.card}>
-        
+      <div className={PokeCard.container}>
+        <div className={PokeCard.card}>
+          <div className={PokeCard.imgContain}>
+            <img src="" alt="" className={PokeCard.img} />
+          </div>
+
+          <div className={PokeCard.content}>
+            <p>ID</p>
+            <p>Pokemon Name</p>
+            <div className={PokeCard.type}>
+              <p>type 1</p>
+              <p>type 2</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
