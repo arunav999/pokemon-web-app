7d8c7e825ab7e6bc3279b6cb432f12877c5de8a0
Arunav Singh <arunavsingh999@gmail.com>
2024-12-25 13:52:48 +0530

    designed card flip

--- src/Components/Contents/PokemonCard.jsx
+++ src/Components/Contents/PokemonCard.jsx
@@ -6,23 +6,31 @@ const PokemonCard = ({ state }) => {
 
   return (
     <>
-      <div className={PokeCard.container}>
-        <div className={PokeCard.card}>
-          <div className={PokeCard.imgContain}>
-            <img
-              src={image}
-              alt={`${pokeDataName} logo`}
-              className={PokeCard.img}
-            />
-          </div>
+      <div className={PokeCard.mainContainer}>
+        <div className={PokeCard.cardContainer}>
+          <div className={PokeCard.card}>
+            {/* CARD-FRONT */}
+            <div className={PokeCard.cardFront}>
+              <div className={PokeCard.imgContain}>
+                <img
+                  src={image}
+                  alt={`${pokeDataName} logo`}
+                  className={PokeCard.img}
+                />
+              </div>
 
-          <div className={PokeCard.content}>
-            <p>{`#${String(pokeDataId || "").padStart(4, "0")}`}</p>
-            <p>{pokeDataName}</p>
-            <div className={PokeCard.type}>
-              <p>{type}</p>
-              <p>type 2</p>
+              <div className={PokeCard.content}>
+                <p>{`#${String(pokeDataId || "").padStart(4, "0")}`}</p>
+                <p>{pokeDataName}</p>
+                <div className={PokeCard.type}>
+                  <p>{type}</p>
+                  <p>type 2</p>
+                </div>
+              </div>
             </div>
+
+            {/* CARD-BACK */}
+            <div className={PokeCard.cardBack}></div>
           </div>
         </div>
       </div>
